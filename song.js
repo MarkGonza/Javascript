1244b26296373fb727d3069f1e0c93b7e6c98aca
MarkGonza <69465477+MarkGonza@users.noreply.github.com>
2020-11-10 12:08:40 +0530

    Update song.js

--- song.js
+++ song.js
@@ -1,20 +1,21 @@
-//Title: This File is a script containing details of a song
-//it can be used to fetch details of a song when needed. 
+/*
+ 
+Keeping Up With the Javascripts - Part 1: ES6
+Homework #1: Data Types
+Title: This File is a script containing details of a song
+it can be used to fetch details of a song when needed. 
+*/
 
-
-// My favorate Song is "Californication" by Red Hot Chili Peppers 
+//My favorate Song is "Californication" by Red Hot Chili Peppers 
 
 
 /*
-
 =================================================
 Please tell me Whats wrong with this on my email. 
 markygonsalves@gamil.com
 This is the fifth time i am submitting it.
 I want to move on to the next topic. 
 =================================================
-
-
 */
 
 
@@ -36,8 +37,23 @@ var recordsSold ="15 Million";
 var IsSongOnYoutube =true;
 var YoutubeViews =749435049;
 
+// Similar artists array
 var similarartist = ["Eagles","Linkin park","Snow patrol","Greenday","Pink Floyd"];
 
+// Object with popular songs by the band
+var Popularsongs = {
+  1999: "Otherside",
+  2006: "Snow (hey oh)",
+  2002: "Can't stop",
+  2016: "Dark neccesities",
+  1998: "Scar tissue",
+  1991: "Under the bridge",
+  2007: "Dani California",
+  2001: "By the way",
+  2011: "The adventures of rain dance maggie",
+ }
+
+// Object with the address of the artists. 
 var Address = {
 building : "Californication",
 street : "Red Hot Chili Peppers street",
@@ -46,7 +62,8 @@ country : "USA",
 INusa : true,
 }
 
-//display some detials
+
+// Console of the above information -
 console.log("Console log :-")
 console.log(title);
 console.log(artist);
@@ -62,47 +79,8 @@ console.log(IsSongOnYoutube);
 console.log(YoutubeViews);
 
 console.log(similarartist);
+console.log(Popularsongs);
+console.log(Popularsongs[2001]);
 console.log(Address);
-
-
-
-var Songdetails = {
-songName : "Californication",
-artist : "Red Hot Chili Peppers",
-genre : "Alternative Rock",
-DurationInSeconds : 300,
-releaseDate : "June 8, 1999",
-GreatSong : true,
-}
-
-
-//display oject with all detials
-console.log(Songdetails);
-console.log(Songdetails.GreatSong);
-
-//display only release date
-console.log(Songdetails.releaseDate);
-
-
-//an array with similarly acclaimed songs of the time
-var similarartists = ["Eagles","Linkin park","Snow patrol","Greenday","Pink Floyd"];
-
-//display array
-console.log( "similar songs of the time are,"+ " " + similarartists);
-
-
-// to upper case
-console.log(artist.toUpperCase());
-
-
-//Constructor pattern for creating Objects. 
-function SongConstructor(title,artist,year,isGood){
-  this.title = title;
-  this.artist =artist;
-  this.year = year;
-  this.isGood = isGood;
-}
-
-var WishYou = new SongConstructor("Wish You Were Here", "Pink Floyd", 1975, true);
 
 
